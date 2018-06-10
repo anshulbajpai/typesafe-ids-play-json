3e84373969a7742987cc59d5541372d2fe0cc126
Anshul Bajpai <bajpai.anshul@gmail.com>
2018-06-10 14:29:28 +0100

    Upgrade typesafe-ids

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,6 +2,6 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
-  lazy val typesafeIds = "com.github.anshulbajpai" %% "typesafe-ids" % "0.2.0"
+  lazy val typesafeIds = "com.github.anshulbajpai" %% "typesafe-ids" % "0.3.0"
   lazy val playJson = "com.typesafe.play" %% "play-json" % "2.6.9"
 }
